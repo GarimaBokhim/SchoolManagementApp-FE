9ffd5c65cc6db8c60855dc74efe3c3e56bb6f8e7
Garima Rai <garimabokhim@outlook.com>
2025-11-27 10:43:33 +0545

    deployment

--- src/utils/instance.tsx
+++ src/utils/instance.tsx
@@ -1,21 +1,17 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Must be set in Vercel!
 });
+
 api.interceptors.request.use(
   (config) => {
-    if (config.url?.includes("/Authentication/login")) {
-      return config;
-    }
-
     if (typeof window !== "undefined") {
       const token = localStorage.getItem("token");
       if (token) {
         config.headers["Authorization"] = `Bearer ${token}`;
       }
     }
-
     return config;
   },
   (error) => {
