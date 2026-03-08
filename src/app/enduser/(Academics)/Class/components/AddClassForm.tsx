69b2d8839c1274c905aae969fee427d11fc7c9e1
Bhabin Sapkota <bhabinsapkota100@gmail.com>
2026-03-08 18:08:29 +0545

    completed

--- src/app/enduser/(Academics)/Class/components/AddClassForm.tsx
+++ src/app/enduser/(Academics)/Class/components/AddClassForm.tsx
@@ -1,50 +1,50 @@
-"use client";
+'use client'
 
-import { SubmitHandler, UseFormReturn, useFieldArray } from "react-hook-form";
-import { InputElement } from "@/components/Input/InputElement";
-import { ButtonElement } from "@/components/Buttons/ButtonElement";
-import { Toast } from "@/components/Toast/toast";
-import { X } from "lucide-react";
-import { IClass } from "../types/IClass";
-import { useAddClass } from "../hooks";
-import toast from "react-hot-toast";
-import useErrorHandler from "@/components/helpers/ErrorHandling";
-import { AppCombobox } from "@/components/Input/ComboBox";
-import { useGetAllSubjects } from "../../Subject/hooks";
+import { SubmitHandler, UseFormReturn, useFieldArray } from 'react-hook-form'
+import { InputElement } from '@/components/Input/InputElement'
+import { ButtonElement } from '@/components/Buttons/ButtonElement'
+import { Toast } from '@/components/Toast/toast'
+import { X } from 'lucide-react'
+import { IClass } from '../types/IClass'
+import { useAddClass } from '../hooks'
+import toast from 'react-hot-toast'
+import useErrorHandler from '@/components/helpers/ErrorHandling'
+import { AppCombobox } from '@/components/Input/ComboBox'
+import { useGetAllSubjects } from '../../Subject/hooks'
 
 type Props = {
-  form: UseFormReturn<IClass>;
-  onClose: () => void;
-};
+  form: UseFormReturn<IClass>
+  onClose: () => void
+}
 
 const AddClassForm = ({ form, onClose }: Props) => {
-  const addClass = useAddClass();
-  const {data: allsubjects} = useGetAllSubjects();
-  const { handleError, clearError } = useErrorHandler();
+  const addClass = useAddClass()
+  const { data: allsubjects } = useGetAllSubjects()
+  const { handleError, clearError } = useErrorHandler()
 
   const { fields, append, remove } = useFieldArray({
-    name: "subjects",
+    name: 'subjects',
     control: form.control,
-  });
+  })
 
   const handleClose = () => {
-    form.reset();
-    onClose();
-  };
+    form.reset()
+    onClose()
+  }
 
   const onSubmit: SubmitHandler<IClass> = async (data) => {
-    clearError();
+    clearError()
     try {
       await toast.promise(addClass.mutateAsync(data), {
-        loading: "Adding Class...",
-        success: "Class added successfully!",
-      });
-      handleClose();
+        loading: 'Adding Class...',
+        success: 'Class added successfully!',
+      })
+      handleClose()
     } catch (error) {
-      const errorMsg = handleError(error);
-      Toast.error(errorMsg);
+      const errorMsg = handleError(error)
+      Toast.error(errorMsg)
     }
-  };
+  }
 
   return (
     <div className="inset-0 flex items-center justify-center w-full h-full">
@@ -65,7 +65,6 @@ const AddClassForm = ({ form, onClose }: Props) => {
           </div>
 
           <form onSubmit={form.handleSubmit(onSubmit)}>
-         
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-start">
               <InputElement
                 label="Class Name"
@@ -74,68 +73,73 @@ const AddClassForm = ({ form, onClose }: Props) => {
                 placeholder="Enter Name"
                 required
               />
+              <InputElement
+                label="classSymbol"
+                form={form}
+                name="classSymbol"
+                placeholder="Enter Class Symbol"
+              />
             </div>
 
             <div className="mt-6">
               <h2 className="font-semibold mb-2">Subjects</h2>
 
               {fields.map((field, index) => (
-               <div
-                key={field.id}
-                className="flex gap-2 mb-2 items-end border-b border-gray-200 pb-2"
-              >
-                <InputElement
-                  label="Subject Name"
-                  form={form}
-                  name={`subjects.${index}.name`}
-                  placeholder="Enter Subject Name"
-                  required
-                  className="w-48"
-                />
-                <InputElement
-                  label="Code"
-                  form={form}
-                  name={`subjects.${index}.code`}
-                  placeholder="Enter Code"
-                  required
-                  className="w-32"
-                />
-                <InputElement
-                  label="Credit Hours"
-                  form={form}
-                  name={`subjects.${index}.creditHours`}
-                  placeholder="Credit Hours"
-                  type="number"
-                  required
-                  className="w-32"
-                />
-                <InputElement
-                  label="Description"
-                  form={form}
-                  name={`subjects.${index}.description`}
-                  placeholder="Description"
-                  className="w-48"
-                />
-                <button
-                  type="button"
-                  onClick={() => remove(index)}
-                  className="text-red-500 mt-6"
+                <div
+                  key={field.id}
+                  className="flex gap-2 mb-2 items-end border-b border-gray-200 pb-2"
                 >
-                  Remove
-                </button>
-              </div>
-
+                  <InputElement
+                    label="Subject Name"
+                    form={form}
+                    name={`subjects.${index}.name`}
+                    placeholder="Enter Subject Name"
+                    required
+                    className="w-48"
+                  />
+                  <InputElement
+                    label="Code"
+                    form={form}
+                    name={`subjects.${index}.code`}
+                    placeholder="Enter Code"
+                    required
+                    className="w-32"
+                  />
+                  <InputElement
+                    label="Credit Hours"
+                    form={form}
+                    name={`subjects.${index}.creditHours`}
+                    placeholder="Credit Hours"
+                    type="number"
+                    required
+                    className="w-32"
+                  />
+                  <InputElement
+                    label="Description"
+                    form={form}
+                    name={`subjects.${index}.description`}
+                    placeholder="Description"
+                    className="w-48"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => remove(index)}
+                    className="text-red-500 mt-6"
+                  >
+                    Remove
+                  </button>
+                </div>
               ))}
 
               <button
                 type="button"
                 onClick={() =>
                   append({
-                    name: "",
-                    code: "",
+                    name: '',
+                    code: '',
                     creditHours: 0,
-                    classId: "",
-                    description: "",
+                    classId: '',
+                    description: '',
                   })
                 }
                 className="mt-2 px-3 py-1 bg-teal-500 text-white rounded"
@@ -151,7 +155,7 @@ const AddClassForm = ({ form, onClose }: Props) => {
         </fieldset>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default AddClassForm;
+export default AddClassForm
--- src/app/enduser/(Academics)/Class/hooks/index.tsx
+++ src/app/enduser/(Academics)/Class/hooks/index.tsx
@@ -1,65 +1,66 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { api } from "@/utils/instance";
-import { IPaginationResponse } from "@/types/IPaginationResponse";
-import {  IClass } from "../types/IClass";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { api } from '@/utils/instance'
+import { IPaginationResponse } from '@/types/IPaginationResponse'
+import { IClass } from '../types/IClass'
 const ClassEndPoints = {
-  getAllClass: "/api/Academics/all-SchoolClass",
-  createClass: "/api/Academics/AddSchoolClass",
-  removeClass: "/api/Academics/DeleteClass",
-  updateClass: "/api/Academics/UpdateSchoolClass",
-  getClassById: "/api/Academics/SchoolClass",
-  filterClassByDate: "/api/Academics/FilterSchoolClass",
-};
+  getAllClass: '/api/Academics/all-SchoolClass',
+  createClass: '/api/Academics/AddSchoolClass',
+  removeClass: '/api/Academics/DeleteClass',
+  updateClass: '/api/Academics/UpdateSchoolClass',
+  getClassById: '/api/Academics/SchoolClass',
+  filterClassByDate: '/api/Academics/FilterSchoolClass',
+}
 
-const queryKey = "Class";
-const filteredClassQuery = "FilteredClass";
+const queryKey = 'Class'
+const filteredClassQuery = 'FilteredClass'
 type ClassRequest = {
-  name: string;
-  subjects?: classsubjects[];
-};
+  name: string
+  classSymbol: number
+  subjects?: classsubjects[]
+}
 type classsubjects = {
-  name: string;
-  code: string;
-  creditHours: number;
-  description: string;
+  name: string
+  code: string
+  creditHours: number
+  description: string
 }
 
 export const useAddClass = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation<IClass, Error, ClassRequest>({
     mutationFn: async (data: ClassRequest): Promise<IClass> => {
-      const response = await api.post(ClassEndPoints.createClass, data);
-      return response.data;
+      const response = await api.post(ClassEndPoints.createClass, data)
+      return response.data
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [queryKey] });
-      queryClient.invalidateQueries({ queryKey: [filteredClassQuery] });
+      queryClient.invalidateQueries({ queryKey: [queryKey] })
+      queryClient.invalidateQueries({ queryKey: [filteredClassQuery] })
     },
     onError: (error) => {
-      console.error("Error adding Class:", error);
+      console.error('Error adding Class:', error)
     },
-  });
-};
+  })
+}
 
 export const useRemoveClass = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation<IClass, Error, string | undefined>({
     mutationFn: async (Id: string | undefined): Promise<IClass> => {
       if (!Id) {
-        throw new Error("Id is required to remove a Class");
+        throw new Error('Id is required to remove a Class')
       }
-      const response = await api.delete(`${ClassEndPoints.removeClass}/${Id}`);
-      return response.data;
+      const response = await api.delete(`${ClassEndPoints.removeClass}/${Id}`)
+      return response.data
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [queryKey] });
-      queryClient.invalidateQueries({ queryKey: [filteredClassQuery] });
+      queryClient.invalidateQueries({ queryKey: [queryKey] })
+      queryClient.invalidateQueries({ queryKey: [filteredClassQuery] })
     },
-  });
-};
+  })
+}
 
 export const useEditClass = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation<
     IClass,
     Error,
@@ -67,38 +68,38 @@ export const useEditClass = () => {
   >({
     mutationFn: async ({ id, data }): Promise<IClass> => {
       if (!id) {
-        throw new Error("Ïd is required to edit Class");
+        throw new Error('Ïd is required to edit Class')
       }
       const response = await api.patch(
         `${ClassEndPoints.updateClass}/${id}`,
         data
-      );
-      return response.data;
+      )
+      return response.data
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [filteredClassQuery] });
-      queryClient.invalidateQueries({ queryKey: [queryKey] });
+      queryClient.invalidateQueries({ queryKey: [filteredClassQuery] })
+      queryClient.invalidateQueries({ queryKey: [queryKey] })
     },
-  });
-};
+  })
+}
 
 export const useGetClassById = (ClassId: string) => {
   return useQuery({
     queryKey: [queryKey, ClassId],
     queryFn: async (): Promise<IClass> => {
       if (!ClassId) {
-        throw new Error("Id is required to get a Class");
+        throw new Error('Id is required to get a Class')
       }
       const response = await api.get<IClass>(
         `${ClassEndPoints.getClassById}/${ClassId}`
-      );
-      return response.data;
+      )
+      return response.data
     },
     enabled: !!ClassId,
     staleTime: 0,
     retry: false,
-  });
-};
+  })
+}
 
 export const useGetAllClass = (params?: string) => {
   return useQuery({
@@ -106,8 +107,8 @@ export const useGetAllClass = (params?: string) => {
     queryFn: async () => {
       const url = params
         ? `${ClassEndPoints.getAllClass}${params}`
-        : `${ClassEndPoints.getAllClass}`;
-      const response = await api.get<IPaginationResponse<IClass>>(url);
+        : `${ClassEndPoints.getAllClass}`
+      const response = await api.get<IPaginationResponse<IClass>>(url)
       return (
         response.data ?? {
           data: [],
@@ -115,10 +116,10 @@ export const useGetAllClass = (params?: string) => {
           isPagination: 1,
           pageSize: 10,
         }
-      );
+      )
     },
-  });
-};
+  })
+}
 
 export const useFilterClassByDate = (params?: string) => {
   return useQuery({
@@ -126,11 +127,11 @@ export const useFilterClassByDate = (params?: string) => {
     queryFn: async () => {
       const url = params
         ? `${ClassEndPoints.filterClassByDate}${params}`
-        : ClassEndPoints.filterClassByDate;
-      const response = await api.get<IPaginationResponse<IClass>>(url);
-      return response.data;
+        : ClassEndPoints.filterClassByDate
+      const response = await api.get<IPaginationResponse<IClass>>(url)
+      return response.data
     },
     staleTime: 0,
     retry: false,
-  });
-};
+  })
+}
